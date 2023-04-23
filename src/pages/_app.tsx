594eb4657134558786cfdcde9925609976f67d85
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2023-04-23 12:49:42 -0500

    Inital setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import { darkTheme } from "@/themes";
+import { UIProvider } from "@/context/ui";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return(
+      <UIProvider>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+      </UIProvider>
+    )
 }
